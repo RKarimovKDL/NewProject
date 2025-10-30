2d2555799a5cbdee093de486bc34a302a1e38db3
RKarimovKDL <RKarimov@kdl.ru>
2025-10-30 14:50:22 +0500

    Задача 3.3

--- Task3.cs
+++ Task3.cs
@@ -1,11 +1,37 @@
 ﻿internal class Task3
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
-        string MyName;
+        Task3_3();
+    }
 
-        MyName = "RKarimov";
+    static void Task3_3()
+    {
+        Console.WriteLine("Как вас зовут?");
+        string Name = Console.ReadLine();
 
-        Console.WriteLine(MyName);
+        Console.WriteLine("Сколько вам лет?");
+        string Age = Console.ReadLine();
+
+        Console.WriteLine("Если ли у вас питомец?");
+        bool Pet = Console.ReadLine();
+
+        Console.WriteLine("Какой у вас размер ноги?");
+        string FootSize = Console.ReadLine();
+
+        Boolean PetType;
+        if (Pet == "Yes" || Pet == "Да")
+        {
+            PetType = true;
+        }
+        else
+        {
+            PetType = false;
+        }
+
+
+        Console.WriteLine("My name is {0}\nMy age is {1}\nDo I have a pet? {2}\nMy shoe size is {3}", Name,Age, PetType, FootSize);
+
+        Console.ReadKey();
     }
 }
